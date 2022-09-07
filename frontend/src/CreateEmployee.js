a41dbfdcfb798efe38e19b7faa7629f5748dbdcd
Daniel Escalona <daniel07escalona@gmail.com>
2022-09-07 11:38:21 -0400

    Add form for new employee

--- frontend/src/CreateEmployee.js
+++ frontend/src/CreateEmployee.js
@@ -1,10 +1,61 @@
 import { useState } from 'react';
 
 function CreateEmployee(){
+  
+  const [formData, setForm] = useState({
+    name: '',
+    dob: '',
+    available: '',
+    address: '',
+    phone: '',
+    position: ''
+
+  })
+
+  function handleChange(event){
+    const name = event.target.name;
+    let value = event.target.value;
+
+    setForm({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    console.log(formData)
+  }
 
   return(
     <div className="mainSection">
-      <p>hola nuevo componente</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Full name:
+        <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+        </label>
+        <label>
+          DOB:
+        <input type="text" name="dob" value={formData.dob} onChange={handleChange}/>
+        </label>
+        <label>
+          Days available to work:
+        <input type="text" name="available" value={formData.available} onChange={handleChange}/>
+        </label>
+        <label>
+          Address:
+        <input type="text" name="address" value={formData.address} onChange={handleChange}/>
+        </label>
+        <label>
+          Phone number:
+        <input type="text" name="phone" value={formData.phone} onChange={handleChange}/>
+        </label>
+        <label>
+          Position:
+        <input type="text" name="position" value={formData.position} onChange={handleChange}/>
+        </label>
+        <input type="submit" value="Submit" />
+     </form>
     </div>
     
   )
