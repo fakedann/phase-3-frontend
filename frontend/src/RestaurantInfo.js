d9850fe82e8d23050ac11949f3f97a88b20cbdcb
Daniel Escalona <daniel07escalona@gmail.com>
2022-09-27 19:50:41 -0400

    Display list of employees

--- frontend/src/RestaurantInfo.js
+++ frontend/src/RestaurantInfo.js
@@ -3,6 +3,12 @@ import { useEffect, useState } from 'react';
 function RestaurantInfo({switchBack, restid}){
 
   const [restInfo, setRest] = useState('')
+  const [listEmployees, setEmployees] = useState([])
+  const [expandOrCollapse, setExpandOrCollapse] = useState('collapse')
+  console.log(listEmployees)
+
+  const displayedEmployees = listEmployees.map( emplObj => <p key={emplObj.id}>Full Name: {emplObj.name}<br></br>  DOB: {emplObj.dob}<br></br> Address: {emplObj.address}<br></br> Phone: {emplObj.phone}<br></br> Position: {emplObj.position}</p>)
+
   
   useEffect(() => {
     fetch(`http://localhost:9292/restaurant/${restid}`)
@@ -17,12 +23,32 @@ function RestaurantInfo({switchBack, restid}){
     switchBack()
   }
 
+  function handleEmployees(){
+    console.log('empl')
+
+    if(expandOrCollapse === 'expand'){
+      setExpandOrCollapse('collapse')
+    }else{
+      fetch(`http://localhost:9292/restaurant/${restid}/employees`)
+      .then((r) => r.json())
+      .then( (data) => {
+        console.log(data[0])
+          setEmployees(data)
+          setExpandOrCollapse('expand')
+      });
+    }
+  
+    
+  }
+
   return(
     <div className="mainSection">
       <div className="restInfo">
         {restInfo}
+        {expandOrCollapse === 'collapse' ? '': <h2>LIST OF EMPLOYEES</h2>}
+        { expandOrCollapse === 'collapse' ? '': displayedEmployees}
       </div>
-      
+      <button className="btnTask" onClick={handleEmployees}>{expandOrCollapse === "collapse" ? "Show All Employees": "Back to General Info"}</button>
       <button className="btnTask" onClick={handleClick}>Back to Menu</button>
     </div>
   )
