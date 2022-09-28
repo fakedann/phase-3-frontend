2e53e1fabc57b2438b32f26e49c71b585b6d4be8
Daniel Escalona <daniel07escalona@gmail.com>
2022-09-28 12:30:34 -0400

    Delete button works on search mode

--- frontend/src/SearchEmployee.js
+++ frontend/src/SearchEmployee.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function SearchEmployee({switchBack, restid}){
 
   const [search, setSearch] = useState('')
   const [employeeInfo, setInfo] = useState('')
+
+  const borrar = () => toast("The employee you selected has been fired and deleted from your records.", {position: toast.POSITION.TOP_CENTER})
 
   function handleSubmit(event){
     event.preventDefault()
@@ -12,7 +16,7 @@ function SearchEmployee({switchBack, restid}){
       .then( (data) => {
         console.log(data)
         if(data !== null){
-          setInfo(<p>Full Name: {data.name}<br></br>  DOB: {data.dob}<br></br> Address: {data.address}<br></br> Phone: {data.phone}<br></br> Position: {data.position}</p>)
+          setInfo(<p>Full Name: {data.name}<br></br>  DOB: {data.dob}<br></br> Address: {data.address}<br></br> Phone: {data.phone}<br></br> Position: {data.position}<br></br><button id={data.id} onClick={handleDelete}>Fire Employee</button></p>)
         }else{
           setInfo('We could not find that employee. Please, try again.')
         }
@@ -23,6 +27,19 @@ function SearchEmployee({switchBack, restid}){
 
   function handleClick(event){
     switchBack()
+  }
+
+  function handleDelete(event){
+    console.log(event.target)
+    fetch(`http://localhost:9292/employees/${event.target.id}/${restid}`, {
+      method: 'DELETE',
+      })
+    .then(res => res.json())
+    .then(res => {
+      borrar()
+      setInfo('')
+    })
+
   }
 
   return(
